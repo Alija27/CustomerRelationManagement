58d561020c4fedaa08515828a428f3679203f573
Alija Ghimire <alijaghimire27@gmail.com>
2022-09-06 15:15:26 +0545

    Added  field in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,6 +20,11 @@ class UserSeeder extends Seeder
             "email" => "admin@admin.com",
             "password" => bcrypt("password"),
             "role" => "admin",
+            "address" => "Nepal",
+            "phonenumber" => "9812919812",
+            "dob" => "2022/09/06",
+            "bloodgroup" => "A+",
+            "post" => "admin",
         ]);
     }
 }
