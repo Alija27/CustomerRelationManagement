c2f91d9f34e777bd81a3ec335655fa09af6a2824
Alija Ghimire <alijaghimire27@gmail.com>
2022-09-07 12:52:39 +0545

    Dashboard created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,37 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@extends("layouts.app")
+@section("main")
+<div class="flex flex-wrap gap-6 lg:gap-4 lg:flex-nowrap mx-6{{-- justify-center gap-6 m-6 mt-6 lg:flex-nowrap sm:flex-row lg:w-8/12 lg:justify-start lg:gap-4 --}}">
+    {{--  <div class="text-2xl text-blue-600">
+        Welcome to dashboard
+     </div>  --}}
+     
+         
+  <div class="flex w-11/12 h-20 p-2 bg-white border-red-100 rounded-md shadow-xl md:w-5/12 lg:w-1/4 ">
+      <span class="items-center px-5 py-5 text-white bg-indigo-600 rounded-md"><i class="fa-solid fa-eye"></i></span>
+      <div class="mx-4 mt-4 ">
+      <div class="antialiased font-bold text-gray-600 md:text-sm">Profile_Views</div>
+      <div class="text-sm text-gray-600">16</div>
+      </div>
+  </div>
+  <div class="flex w-11/12 h-20 p-2 bg-white border-red-100 rounded-md shadow-xl md:w-5/12 lg:w-1/4 ">
+      <span class="items-center px-5 py-5 text-white bg-indigo-600 rounded-md"><i class="fa-solid fa-eye"></i></span>
+      <div class="mx-4 mt-4 ">
+      <div class="antialiased font-bold text-gray-600 md:text-sm">Followers</div>
+      <div class="text-sm text-gray-600">16</div>
+      </div>
+  </div>
+  <div class="flex w-11/12 h-20 p-2 bg-white border-red-100 rounded-md shadow-xl md:w-5/12 lg:w-1/4 ">
+      <span class="items-center px-5 py-5 text-white bg-indigo-600 rounded-md"><i class="fa-solid fa-eye"></i></span>
+      <div class="mx-4 mt-4 ">
+      <div class="antialiased font-bold text-gray-600 md:text-sm ">Profile_Views</div>
+      <div class="text-sm text-gray-600">16</div>
+      </div>
+  </div>
+  <div class="flex w-11/12 h-20 p-2 bg-white border-red-100 rounded-md shadow-xl md:w-5/12 lg:w-1/4 ">
+      <span class="items-center px-5 py-5 text-white bg-indigo-600 rounded-md"><i class="fa-solid fa-eye"></i></span>
+      <div class="mx-4 mt-4 ">
+      <div class="antialiased font-bold text-gray-600 md:text-sm ">View</div>
+      <div class="text-sm text-gray-600">16</div>
+      </div>
+  </div>
+  @endsection
