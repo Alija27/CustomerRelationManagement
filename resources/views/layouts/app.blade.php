d42ca8a37abc6cfe4fe33b93839c38d6a286c997
Alija Ghimire <alijaghimire27@gmail.com>
2022-09-12 19:44:15 +0545

    Nav edited

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,15 +10,15 @@
         <!-- Fonts -->
         <link  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
+@yield('css')
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+           {{--  @include('layouts.navigation') --}}
             <div class="flex">
-                <nav class="hidden h-screen bg-white shadow-lg lg:w-2.8/12 lg:block">
+                <nav class="hidden h-screen bg-white shadow-lg lg:w-3/12 lg:block">
                    <div class="p-6 mx-6">
                        <ul class="text-xl antialiased font-bold text-blue-500">
                            <li><i class="pr-2 fa-solid fa-user"></i>
@@ -27,30 +27,36 @@
                            
                        </ul>
                    </div>
-                   <div class="px-6 py-2 mx-2">
-                       <ul class="antialiased font-bold text-gray-600 text-md ">
+                   <div class="px-2 py-2">
+                       <ul class="pl-2 antialiased font-bold text-md ">
                            <li class="px-2 py-2 my-2 rounded-md hover:bg-indigo-600 hover:text-white">
                              <i class="pr-2 fa-solid fa-gauge-high"></i>
                             Dashboard
                            </li>
-                          {{--  <a href="{{route('users.index')}}">
-                            <li class="px-2 py-2 @if (Request::url('/users')) {{'bg-indigo-600 text-white'}} @endif rounded-md hover:bg-indigo-600 hover:text-white">
+                           <a href="{{route('users.index')}}">
+                            <li class="px-2 py-2  {{ request()->routeIs('users*') ?'bg-indigo-600 text-white':''}} rounded-md hover:bg-indigo-600 hover:text-white">
                                 <i class="pr-2 fa-solid fa-square"></i>
                                   Users
                               </li>
-                           </a> --}}
-                           <li class="px-2 py-2 my-2 rounded-md hover:bg-indigo-600 hover:text-white">
+                           </a> 
+                           <a href="{{route('clients.index')}}">
+                           <li class="px-2 py-2 my-2 {{ request()->routeIs('clients*') ?'bg-indigo-600 text-white':''}} rounded-md hover:bg-indigo-600 hover:text-white">
                              <i class="pr-2 fa-solid fa-box"></i>
-                               Extra Components
+                               Clients
                            </li>
+                           </a> 
+                           <a href="{{route('departments.index')}}">
                            <li class="px-2 py-2 my-2 rounded-md hover:bg-indigo-600 hover:text-white">
                              <i class="pr-2 fa-solid fa-align-left"></i>
-                               Layouts
+                               Departments
                            </li>
+                          </a> 
+                          <a href="{{route('airlines.index')}}">
                            <li class="px-2 py-2 my-2 rounded-md hover:bg-indigo-600 hover:text-white">
                              <i class="pr-2 fa-brands fa-adversal"></i>
-                               Form Elements
+                               Airlines
                            </li>
+                          </a> 
                            <li class="px-2 py-2 my-2 rounded-md hover:bg-indigo-600 hover:text-white">
                              <i class="pr-2 fa-brands fa-creative-commons-nd"></i>
                                Form Layout
@@ -73,7 +79,7 @@
                    </div>
                 </nav>
                 
-               <div class="justify-center w-full mt-6 sm:flex-row lg:w-8/12 lg:justify-start ">
+               <div class="justify-center w-full m-6 sm:flex-row ">
                  @yield('main')
                </div>
                     
@@ -83,5 +89,6 @@
              </div> 
             </div> 
         </div>
+        @yield('js')
     </body>
 </html>
