3e0a2842fcd74ba1f6dda1cad231bcd9e1f2d3f5
Alija Ghimire <alijaghimire27@gmail.com>
2022-09-12 19:31:08 +0545

    CLient create page created

---
+++ resources/views/clients/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+@section('main')
+
+
+    <div  class="w-full bg-white border border-gray-200 shadow-md">
+        <div class="flex justify-between mb-6 border-b border-gray-200">
+            <span class="m-1 mx-4 my-4 text-2xl font-bold">Create Client</span>
+            <a href="{{route('clients.index')}}">
+            <button class="p-2 px-4 mx-4 my-4 text-white bg-indigo-600 rounded-lg">
+           <- Go Back
+            </button>
+            </a>
+            
+        </div>
+      <form class="w-11/12 p-6 m-6 overflow-auto bg-white rounded-md " method="POST" action={{route('clients.store')}}>
+        @csrf
+        @if ($errors->any())
+        {{$errors}}
+    @endif
+        <div class="mb-6">
+            <label class="block font-bold text-gray-600 text-md">Name</label>
+            <input  class="w-full border border-gray-200 " type="text" name="name" id="name">
+        </div>
+        <div class="mb-6">
+            <label class="block font-bold text-gray-600 text-md">Email</label>
+            <input  class="w-full border border-gray-200" type="text" name="email" id="email">
+        </div>
+        <div class="mb-6">
+            <label class="block font-bold text-gray-600 text-md">Address</label>
+            <input  class="w-full border border-gray-200" type="text" name="address" id="address">
+        </div>
+        <div class="mb-6">
+            <label class="block font-bold text-gray-600 text-md">Phonenumber</label>
+            <input  class="w-full border border-gray-200" type="number" name="phonenumber" id="phonenumber">
+        </div>
+        {{-- <div class="mb-6">
+            <label class="block font-bold text-gray-600 text-md">Added By</label>
+            <input  class="w-full border border-gray-200" type="text" name="added_by" id="added_by">     
+        </div> --}}
+        
+        <div class="mb-6">
+            <Button class="p-2 px-4 text-white bg-indigo-600 rounded-xl">Create</Button>
+        </div>
+      </form>
+</div>
+@endsection
