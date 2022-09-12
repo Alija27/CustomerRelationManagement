c26f0b1ae60465d9632c28162e43466d0649a39c
Alija Ghimire <alijaghimire27@gmail.com>
2022-09-12 19:44:51 +0545

    User Client Department Airline Attendence route created

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,19 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'admin'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+Route::resource("users", \App\Http\Controllers\UserController::class);
+Route::delete('user/delete', [\App\Http\Controllers\UserController::class, 'deleteUser'])->name(('users.delete'));
+
+Route::resource("clients", \App\Http\Controllers\ClientController::class);
+Route::delete('client/delete', [\App\Http\Controllers\ClientController::class, 'deleteClient'])->name(('clients.delete'));
+
+Route::resource("departments", \App\Http\Controllers\DepartmentController::class);
+Route::delete('department/delete', [\App\Http\Controllers\DepartmentController::class, 'deleteDepartment'])->name(('departments.delete'));
+
+Route::resource("airlines", \App\Http\Controllers\AirlineController::class);
+Route::delete('airline/delete', [\App\Http\Controllers\AirlineController::class, 'deleteAirline'])->name(('airlines.delete'));
+
+Route::resource("attendences", \App\Http\Controllers\AttendenceController::class);
 require __DIR__ . '/auth.php';
