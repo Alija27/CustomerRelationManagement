6ffb93f4194fa31f5c45eab0212508cc1c81f16b
Alija Ghimire <alijaghimire27@gmail.com>
2022-09-13 12:25:04 +0545

    clockout done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
             @csrf
         <button onclick="show()" class="p-2 px-6 mr-2 text-white bg-indigo-600 rounded hover:bg-green-600"><i class="fa-solid fa-stopwatch"></i><i class="m-1 fa-solid fa-arrow-right"></i>Clock In</button>
         {{-- </form> --}}
-        <button  class="p-2 px-6 ml-2 text-white bg-indigo-600 rounded hover:bg-red-600"><i class="fa-solid fa-arrow-left"><i class="m-1 fa-solid fa-stopwatch"></i></i>Clock Out</button>
+        <button onclick="out()" class="p-2 px-6 ml-2 text-white bg-indigo-600 rounded hover:bg-red-600"><i class="fa-solid fa-arrow-left"><i class="m-1 fa-solid fa-stopwatch"></i></i>Clock Out</button>
        
     </div>
 </div>
@@ -67,6 +67,27 @@
         </div>
     </div>
 </div>
+<div id="cout" class="fixed top-0 bottom-0 left-0 right-0 hidden bg-black bg-opacity-10 backdrop-blur-lg">
+    
+    <div class="flex items-center justify-center">
+        
+        <div class="px-10 py-10 font-bold bg-white rounded-lg shadow-lg mt-[15%]">
+            <div class="font-bold text-center">
+                Remarks
+            </div>
+            @if (isset($attendence))    
+            <form action="{{route('attendences.update', $attendence->id)}}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="flex flex-col mt-8 space-x-5">
+                <textarea class="border-gray-800 rounded border-1" name="reason" id="reason"></textarea>
+                <button type="submit" class="p-2 mt-1 text-white bg-green-600 rounded-md">Submit</button>
+                </div>
+            </form>
+            @endif
+        </div>
+    </div>
+</div>
   @endsection
   @section('js')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
@@ -74,7 +95,11 @@
 
 <script>
     function show(){
-        $('#ri').removeClass("hidden");
+        $("#ri").removeClass("hidden");
+    }
+    
+    function out(){
+        $("#cout").removeClass("hidden");
     }
 </script>
 @endsection
