f92e00ad3f3863fe172411bba69f3aa11d37354d
Alija Ghimire <alijaghimire27@gmail.com>
2022-10-11 08:24:38 +0545

    admin view attendence created

--- resources/views/admin/attendences/show.blade.php
+++ resources/views/admin/attendences/show.blade.php
@@ -1,122 +1,120 @@
 @extends('layouts.app')
 @section('css')
-<link rel="stylesheet"href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-<style>
-    body{
-        overflow: hidden;
-    }
-    .dataTables_length select{
-        width: 4em;
-        height: 2em;
-    }
-    .dataTables_filter{
-        margin-bottom: 1em
-    }
-    .dataTables_filter input{
-        height: 2em
-    }
-    .dataTables_wrapper{
-        margin: 0 20px
-    }
-</style>
+    <link rel="stylesheet"href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <style>
+        body {
+            overflow: hidden;
+        }
+
+        .dataTables_length select {
+            width: 4em;
+            height: 2em;
+        }
+
+        .dataTables_filter {
+            margin-bottom: 1em
+        }
+
+        .dataTables_filter input {
+            height: 2em
+        }
+
+        .dataTables_wrapper {
+            margin: 0 20px
+        }
+    </style>
 @endsection
 
 
 @section('main')
-@if (session('message'))
-    {{session('message')}}
-@endif
-      <div  class="w-full overflow-auto bg-white border border-gray-200 shadow-md">
-          <div class="flex justify-between mb-6 border-b border-gray-200">
-              <span class="m-1 mx-4 my-4 text-2xl font-bold">Admin Attendences</span>
-              <a href="{{route('users.create')}}">
-              <button class="p-2 px-4 mx-4 my-4 text-white bg-indigo-600 rounded-lg">
-               Add New
-              </button>
-              </a>
-          </div>
-          <table class="max-w-full mx-5 my-5 display" id="myTable">
-              <thead class="border-b">
-                <tr  class="bg-indigo-600 ">
+    @if (session('message'))
+        {{ session('message') }}
+    @endif
+    <div class="w-full overflow-auto bg-white border border-gray-200 shadow-md">
+        <div class="flex justify-between mb-6 border-b border-gray-200">
+            <span class="m-1 mx-4 my-4 text-2xl font-bold">Individual Attendences</span>
+            <a href="{{ route('users.create') }}">
+                <button class="p-2 px-4 mx-4 my-4 text-white bg-indigo-600 rounded-lg">
+                    <i class="mr-2 fa-solid fa-arrow-left"></i> GoBack
+                </button>
+            </a>
+        </div>
+        <table class="max-w-full mx-5 my-5 display" id="myTable">
+            <thead class="border-b">
+                <tr class="bg-indigo-600 ">
                     <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
                         Date
                     </th>
                     <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
                         Entry Time
-                  </th>
-                  <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
-                      Exit Time
-                  </th>
-                  <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
-                      Total Time Worked
-                  </th>
-                  <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
-                      Late Entry Reason
-                  </th>
-                  <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
-                     Early Exit Reason
-                  </th>
+                    </th>
+                    <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
+                        Exit Time
+                    </th>
+                    <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
+                        Total Time Worked
+                    </th>
+                    <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
+                        Late Entry Reason
+                    </th>
+                    <th sope="col" class="px-6 py-4 text-sm font-medium text-white">
+                        Early Exit Reason
+                    </th>
                 </tr>
-              </thead>
-              <tbody>
-            @foreach($attendences as $attendence)
-            <tr class="border-b">
-                <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
-            {{$attendence->date}}
-          </td>
-         
-              <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
-          {{$attendence->clock_in}}
-        </td>
-        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
-          {{$attendence->clock_out}}
-        </td>
-        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
-          {{$attendence->total_time}}
-        </td>
-        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
-          {{$attendence->late_entry}}
-        </td>
-        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
-          {{$attendence->early_exit}}
-        </td>
-        
-           
-  
-          </tr>
-          @endforeach
-    </tbody>
+            </thead>
+            <tbody>
+                @foreach ($attendences as $attendence)
+                    <tr class="border-b">
+                        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
+                            {{ $attendence->date }}
+                        </td>
+
+                        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
+                            {{ $attendence->clock_in }}
+                        </td>
+                        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
+                            {{ $attendence->clock_out }}
+                        </td>
+                        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
+                            {{ $attendence->total_time }}
+                        </td>
+                        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
+                            {{ $attendence->late_entry }}
+                        </td>
+                        <td class="text-sm font-medium text-gray-900 whitespace-nowrap">
+                            {{ $attendence->early_exit }}
+                        </td>
+
+
+
+                    </tr>
+                @endforeach
+            </tbody>
 
         </table>
-     
-
-</div>
 
 
-
-
+    </div>
 @endsection
 
 @section('js')
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    
-<script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-</script>
+    <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script>
-                $(document).ready( function () { 
+        $(document).ready(function() {
             $('#myTable').DataTable();
-        } );
-        
-        function show($id){
+        });
+
+        function show($id) {
             document.getElementById('user_id').value = $id;
 
             $('#deleteModal').removeClass('hidden');
         }
-        function hide(){
+
+        function hide() {
             $('#deleteModal').addClass('hidden');
         }
-
-       
     </script>
 @endsection
