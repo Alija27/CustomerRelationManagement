96a7a6505407c3ddf94207cc159da10f1785659e
Alija Ghimire <alijaghimire27@gmail.com>
2022-10-11 14:58:18 +0545

    all count in dashboard

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -6,7 +6,10 @@ use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Leave;
 use App\Models\Client;
+use App\Models\Income;
+use App\Models\Ticket;
 use App\Models\Attendence;
+use App\Models\Expenditure;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
@@ -16,10 +19,17 @@ class SiteController extends Controller
         $attendence = Attendence::whereDate('date', Carbon::today())->where('user_id', auth()->user()->id)->first();
 
         $attendences = Attendence::where('user_id', auth()->user()->id)->count();
+        $leaves = Leave::where('user_id', auth()->user()->id)->count();
+        $clients = Client::all()->count();
+        $this_month_attendence = Attendence::whereYear('date', Carbon::now()->year)->whereMonth('date', Carbon::now()->month)->count();
+        $client_birthday = Client::whereMonth('dob', Carbon::now()->month)->whereDay('dob', '>=', Carbon::now()->day)->orderByRaw('DAYOFYEAR(dob)')->count();
+        $user_birthday = User::whereMonth('dob', Carbon::now()->month)->whereDay('dob', '>=', Carbon::now()->day)->orderByRaw('DAYOFYEAR(dob)')->count();
+        $today_ticket = Ticket::whereYear('date', Carbon::now()->year)->whereMonth('date', Carbon::now()->month)->whereDay('date', Carbon::now()->day)->count();
+        $upcomming_ticket = Ticket::whereYear('date', Carbon::now()->year)->whereMonth('date', Carbon::now()->month)->count();
+        $today_income = Income::whereYear('date', Carbon::now()->year)->whereMonth('date', Carbon::now()->month)->whereDay('date', Carbon::now())->sum('amount');
+        $today_expenditure = Expenditure::whereYear('date', Carbon::now()->year)->whereMonth('date', Carbon::now()->month)->whereDay('date', Carbon::now())->sum('amount');
 
-
-
-        return view('dashboard', compact("attendences", 'attendence'));
+        return view('dashboard', compact("attendences", 'attendence', 'leaves', 'clients', 'this_month_attendence', 'client_birthday', 'user_birthday', 'today_ticket', 'upcomming_ticket', 'today_income'));
     }
 
 
