bad9464556cc5d6d97570b1aa8d18649169c75e5
Alija Ghimire <alijaghimire27@gmail.com>
2022-10-11 12:08:51 +0545

    department updated

--- resources/views/departments/create.blade.php
+++ resources/views/departments/create.blade.php
@@ -1,30 +1,29 @@
 @extends('layouts.app')
 @section('main')
-
-
-    <div  class="w-full bg-white border border-gray-200 shadow-md">
+    <div class="w-full bg-white border border-gray-200 shadow-md">
         <div class="flex justify-between mb-6 border-b border-gray-200">
             <span class="m-1 mx-4 my-4 text-2xl font-bold">Create Department</span>
-            <a href="{{route('departments.index')}}">
-            <button class="p-2 px-4 mx-4 my-4 text-white bg-indigo-600 rounded-lg">
-               <- Go Back
-            </button>
+            <a href="{{ route('departments.index') }}">
+                <button class="p-2 px-4 mx-4 my-4 text-white bg-indigo-600 rounded-lg">
+                    <i class="mr-2 fa-solid fa-arrow-left"></i> Go Back
+                </button>
             </a>
         </div>
-      <form class="w-11/12 p-6 m-6 overflow-auto bg-white rounded-md " method="POST" action={{route('departments.store')}}>
-        @csrf
-        @if ($errors->any())
-        {{$errors}}
-    @endif
-        <div class="mb-6">
-            <label class="block font-bold text-gray-600 text-md">Title</label>
-            <input  class="w-full border border-gray-200 " type="text" name="title" id="title">
-        </div>
-       
-        
-        <div class="mb-6">
-            <Button class="p-2 px-4 text-white bg-indigo-600 rounded-xl">Create</Button>
-        </div>
-      </form>
-</div>
+        <form class="w-11/12 p-6 m-6 overflow-auto bg-white rounded-md " method="POST"
+            action={{ route('departments.store') }}>
+            @csrf
+            @if ($errors->any())
+                {{ $errors }}
+            @endif
+            <div class="mb-6">
+                <label class="block font-bold text-gray-600 text-md">Title</label>
+                <input class="w-full border border-gray-200 " type="text" name="title" id="title">
+            </div>
+
+
+            <div class="mb-6">
+                <Button class="p-2 px-4 text-white bg-indigo-600 rounded-xl">Create</Button>
+            </div>
+        </form>
+    </div>
 @endsection
