416f4f428a2ca340fdad26e7e7f1ee0da4dead28
Alija Ghimire <alijaghimire27@gmail.com>
2022-10-13 21:39:06 +0545

    alert route and middleware added

--- routes/web.php
+++ routes/web.php
@@ -21,56 +21,61 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [SiteController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
 
-Route::resource("users", \App\Http\Controllers\UserController::class);
-Route::delete('user/delete', [\App\Http\Controllers\UserController::class, 'deleteUser'])->name(('users.delete'));
-Route::get("user/task/{id}", [\App\Http\Controllers\UserController::class, 'viewTask'])->name("user.task");
+    Route::get('/dashboard', [SiteController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
-Route::resource("clients", \App\Http\Controllers\ClientController::class);
-Route::delete('client/delete', [\App\Http\Controllers\ClientController::class, 'deleteClient'])->name(('clients.delete'));
+    Route::resource("users", \App\Http\Controllers\UserController::class);
+    Route::delete('user/delete', [\App\Http\Controllers\UserController::class, 'deleteUser'])->name(('users.delete'));
+    Route::get("user/task/{id}", [\App\Http\Controllers\UserController::class, 'viewTask'])->name("user.task");
 
-Route::resource("departments", \App\Http\Controllers\DepartmentController::class);
-Route::delete('department/delete', [\App\Http\Controllers\DepartmentController::class, 'deleteDepartment'])->name(('departments.delete'));
+    Route::resource("clients", \App\Http\Controllers\ClientController::class);
+    Route::delete('client/delete', [\App\Http\Controllers\ClientController::class, 'deleteClient'])->name(('clients.delete'));
 
-Route::resource("airlines", \App\Http\Controllers\AirlineController::class);
-Route::delete('airline/delete', [\App\Http\Controllers\AirlineController::class, 'deleteAirline'])->name(('airlines.delete'));
+    Route::resource("departments", \App\Http\Controllers\DepartmentController::class);
+    Route::delete('department/delete', [\App\Http\Controllers\DepartmentController::class, 'deleteDepartment'])->name(('departments.delete'));
 
-Route::resource("attendences", \App\Http\Controllers\AttendenceController::class);
+    Route::resource("airlines", \App\Http\Controllers\AirlineController::class);
+    Route::delete('airline/delete', [\App\Http\Controllers\AirlineController::class, 'deleteAirline'])->name(('airlines.delete'));
 
-Route::resource("leaves", \App\Http\Controllers\LeaveController::class);
+    Route::resource("attendences", \App\Http\Controllers\AttendenceController::class);
 
-Route::get("admin/attendences", [\App\Http\Controllers\Admin\AttendenceController::class, "index"])->name('admin.attendence');
-Route::get("admin/attendences/{id}", [\App\Http\Controllers\Admin\AttendenceController::class, "show"])->name("admin.attendence.show");
+    Route::resource("leaves", \App\Http\Controllers\LeaveController::class);
 
-Route::get("admin/leaves", [\App\Http\Controllers\Admin\LeaveController::class, "index"])->name('admin.leaves');
-Route::post("admin/approved", [\App\Http\Controllers\Admin\LeaveController::class, "approved"])->name('admin.leave.approved');
-Route::post("admin/rejected", [\App\Http\Controllers\Admin\LeaveController::class, "rejected"])->name('admin.leave.rejected');
-Route::get("admin/leaves/{id}", [\App\Http\Controllers\Admin\LeaveController::class, "show"])->name('admin.leave.show');
+    Route::get("admin/attendences", [\App\Http\Controllers\Admin\AttendenceController::class, "index"])->name('admin.attendence');
+    Route::get("admin/attendences/{id}", [\App\Http\Controllers\Admin\AttendenceController::class, "show"])->name("admin.attendence.show");
 
-Route::resource("purposes", \App\Http\Controllers\PurposeController::class);
-Route::delete('purpose/delete', [\App\Http\Controllers\PurposeController::class, 'deletePurpose'])->name(('purposes.delete'));
+    Route::get("admin/leaves", [\App\Http\Controllers\Admin\LeaveController::class, "index"])->name('admin.leaves');
+    Route::post("admin/approved", [\App\Http\Controllers\Admin\LeaveController::class, "approved"])->name('admin.leave.approved');
+    Route::post("admin/rejected", [\App\Http\Controllers\Admin\LeaveController::class, "rejected"])->name('admin.leave.rejected');
+    Route::get("admin/leaves/{id}", [\App\Http\Controllers\Admin\LeaveController::class, "show"])->name('admin.leave.show');
 
-Route::resource("tasks", \App\Http\Controllers\TaskController::class);
-Route::get("task/assignTask/{id}", [\App\Http\Controllers\TaskController::class, 'assignTask'])->name(('tasks.assignTask'));
-Route::delete('task/delete', [\App\Http\Controllers\TaskController::class, 'deleteTask'])->name(('tasks.delete'));
-Route::get('task/mytask', [\App\Http\Controllers\TaskController::class, 'myTask'])->name(('task.mytask'));
-Route::post('task/pending/{id}', [\App\Http\Controllers\TaskController::class, 'pending'])->name(('task.pending'));
-Route::post('task/processing/{id}', [\App\Http\Controllers\TaskController::class, 'processing'])->name(('task.processing'));
-Route::post('task/complete/{id}', [\App\Http\Controllers\TaskController::class, 'complete'])->name(('task.complete'));
+    Route::resource("purposes", \App\Http\Controllers\PurposeController::class);
+    Route::delete('purpose/delete', [\App\Http\Controllers\PurposeController::class, 'deletePurpose'])->name(('purposes.delete'));
 
-Route::get("birthday/users", [\App\Http\Controllers\SiteController::class, 'birthday'])->name("birthday.users");
-Route::get("birthday/clients", [\App\Http\Controllers\SiteController::class, 'clientbirthday'])->name("birthday.clients");
+    Route::resource("tasks", \App\Http\Controllers\TaskController::class);
+    Route::get("task/assignTask/{id}", [\App\Http\Controllers\TaskController::class, 'assignTask'])->name(('tasks.assignTask'));
+    Route::delete('task/delete', [\App\Http\Controllers\TaskController::class, 'deleteTask'])->name(('tasks.delete'));
+    Route::get('task/mytask', [\App\Http\Controllers\TaskController::class, 'myTask'])->name(('task.mytask'));
+    Route::post('task/pending/{id}', [\App\Http\Controllers\TaskController::class, 'pending'])->name(('task.pending'));
+    Route::post('task/processing/{id}', [\App\Http\Controllers\TaskController::class, 'processing'])->name(('task.processing'));
+    Route::post('task/complete/{id}', [\App\Http\Controllers\TaskController::class, 'complete'])->name(('task.complete'));
 
-Route::resource("tickets", \App\Http\Controllers\TicketController::class);
-Route::get("ticket/domestic", [\App\Http\Controllers\TicketController::class, 'index'])->name("ticket.domestic");
-Route::get("ticket/international", [\App\Http\Controllers\TicketController::class, 'index'])->name("ticket.international");
-Route::get("ticket/today", [\App\Http\Controllers\TicketController::class, 'index'])->name("ticket.today");
+    Route::get("birthday/users", [\App\Http\Controllers\SiteController::class, 'birthday'])->name("birthday.users");
+    Route::get("birthday/clients", [\App\Http\Controllers\SiteController::class, 'clientbirthday'])->name("birthday.clients");
 
-Route::resource("incomes", \App\Http\Controllers\IncomeController::class);
-Route::get("income/total", [\App\Http\Controllers\IncomeController::class, 'calculateIncome'])->name("income.total");
+    Route::resource("tickets", \App\Http\Controllers\TicketController::class);
+    Route::get("ticket/domestic", [\App\Http\Controllers\TicketController::class, 'index'])->name("ticket.domestic");
+    Route::get("ticket/international", [\App\Http\Controllers\TicketController::class, 'index'])->name("ticket.international");
+    Route::get("ticket/today", [\App\Http\Controllers\TicketController::class, 'index'])->name("ticket.today");
 
-Route::resource("expenditures", \App\Http\Controllers\ExpenditureController::class);
+    Route::resource("incomes", \App\Http\Controllers\IncomeController::class);
+    Route::get("income/total", [\App\Http\Controllers\IncomeController::class, 'calculateIncome'])->name("income.total");
 
+    Route::resource("expenditures", \App\Http\Controllers\ExpenditureController::class);
+});
 
+Route::get('alert', function () {
+    return view('alert');
+});
 require __DIR__ . '/auth.php';
