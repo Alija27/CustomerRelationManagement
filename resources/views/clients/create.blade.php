1a72c385cfb38e57c4831ed63c04f95abeb268cf
Alija Ghimire <alijaghimire27@gmail.com>
2022-10-22 16:49:33 +0545

    displayed error

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -11,28 +11,56 @@
         </div>
         <form class="w-11/12 p-6 m-6 overflow-auto bg-white rounded-md " method="POST" action={{ route('clients.store') }}>
             @csrf
-            @if ($errors->any())
-                {{ $errors }}
-            @endif
+
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Name</label>
-                <input class="w-full border border-gray-200 " type="text" name="name" id="name">
+                <input class="w-full border border-gray-200" value="{{ old('name') }}" type="text" name="name"
+                    id="name">
+                <div class="text-red-700 ">
+                    @error('name')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Email</label>
-                <input class="w-full border border-gray-200" type="text" name="email" id="email">
+                <input class="w-full border border-gray-200" value="{{ old('email') }}" type="text" name="email"
+                    id="email">
+                <div class="text-red-700 ">
+                    @error('email')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Address</label>
-                <input class="w-full border border-gray-200" type="text" name="address" id="address">
+                <input class="w-full border border-gray-200" value="{{ old('address') }}" type="text" name="address"
+                    id="address">
+                <div class="text-red-700 ">
+                    @error('address')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Phonenumber</label>
-                <input class="w-full border border-gray-200" type="number" name="phonenumber" id="phonenumber">
+                <input class="w-full border border-gray-200" value="{{ old('phonenuber') }}" type="number"
+                    name="phonenumber" id="phonenumber">
+                <div class="text-red-700 ">
+                    @error('phonenumber')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Date of birth</label>
-                <input class="w-full border border-gray-200 " type="date" name="dob" id="dob">
+                <input class="w-full border border-gray-200 " value="{{ old('dob') }}" type="date" name="dob"
+                    id="dob">
+                <div class="text-red-700 ">
+                    @error('dob')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
 
 
