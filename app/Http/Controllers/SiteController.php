d39edb7c0ce56f9a2c40c0eed11569c11676264c
Alija Ghimire <alijaghimire27@gmail.com>
2022-10-22 14:36:01 +0545

    data for line chart pie char and bar chart

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use App\Models\Task;
 use App\Models\User;
 use App\Models\Leave;
 use App\Models\Client;
@@ -11,6 +12,7 @@ use App\Models\Ticket;
 use App\Models\Attendence;
 use App\Models\Expenditure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SiteController extends Controller
 {
@@ -45,8 +47,98 @@ class SiteController extends Controller
             $user->remaining = $remainingday;
         }
 
+        // For Chart
+        $before7days = \Carbon\Carbon::today()->subDays(7); // today - 7 days
 
-        return view('dashboard', compact("attendences", 'attendence', 'leaves', 'clients', 'this_month_attendence', 'client_birthday_total', 'user_birthday_total', 'today_ticket', 'upcomming_ticket', 'today_income', 'u_birthday', 'c_birthday', 'today_expenditure'));
+        $last7daysnames = []; // retrieve last 7 days names in this array
+        for ($i = 7; $i > 0; $i--) {
+            $last7daysnames[] = (\Carbon\Carbon::today()->subDays($i)->format('l'));
+        }
+
+        // Get total income and expenditure for previous 7 days
+        $last7daysincome = Income::selectRaw('sum(amount) as total_income, DAYNAME(created_at) as day')->where('created_at', '>=', $before7days)->groupBy('day')->orderBy('created_at')->get();
+        $last7daysexp = Expenditure::selectRaw('sum(amount) as total_expenditure, DAYNAME(created_at) as day')->where('created_at', '>=', $before7days)->groupBy('day')->orderBy('created_at')->get();
+        // [sunday, monday, tuesday] days
+        // [
+        //     {
+        //         total_income => 2000,
+        //         day => sunday
+        //     }
+        //     {
+        //         total_income => 2000,
+        //         day => monday
+        //     } 
+        //     {
+        //         total_income => 2000,
+        //         day => wednesday
+        //     }
+        // ]
+        // get toatal income of every day in array
+        $last7daysincomeAmount = collect($last7daysnames)->map(function ($name) use ($last7daysincome) {
+            // $name = sunday
+            foreach ($last7daysincome as $income) {
+                if ($income->day == $name) {
+                    return $income->total_income;
+                }
+            }
+            return 0;
+        });
+        // $last7daysincomeAmount = [2000]
+
+        // get total expenditure of every day in array
+        $last7daysexpAmount = collect($last7daysnames)->map(function ($name) use ($last7daysexp) {
+            foreach ($last7daysexp as $exp) {
+                if ($exp->day == $name) {
+                    return $exp->total_expenditure;
+                }
+            }
+            return 0;
+        });
+
+        // final result for last 7 days income/expenditure report 
+        $finalIncomeExpenditureReport = [
+            'days' => $last7daysnames,
+            'incomes' => $last7daysincomeAmount,
+            'expenditures' => $last7daysexpAmount
+        ];
+
+
+        // Pie chart
+
+        $task_pending_count = Task::where("user_id", Auth::id())->where('status', '=', "pending")->count();
+        $task_processing_count = Task::where("user_id", Auth::id())->where('status', '=', "processing")->count();
+        $task_completed_count = Task::where("user_id", Auth::id())->where('status', '=', "completed")->count();
+        $total_tasks = Task::count();
+
+        $pending_task_percent = (float)((float)$task_pending_count / $total_tasks) * 100;
+        $processing_task_percent = (float)((float)$task_processing_count / $total_tasks) * 100;
+        $completed_task_percent = (float)((float)$task_completed_count / $total_tasks) * 100;
+
+        $finalTaskReport = json_encode([
+            [
+                "name" => "Pending",
+                "y" => $pending_task_percent
+            ],
+            [
+                "name" => "Processing",
+                "y" => $processing_task_percent
+            ],
+            [
+                "name" => "Completed",
+                "y" => $completed_task_percent
+            ]
+        ]);
+
+        /*         $last12month = Carbon::now()->subMonth(12)->format('M');
+ */        // dd($last12month);
+        $months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        $attendence_bar = array();
+        foreach ($months as $month) {
+            array_push($attendence_bar, Attendence::where("user_id", auth()->user()->id)->whereYear('date', Carbon::now()->year)->whereMonth('date', Carbon::parse($month))->count());
+        }
+        $finalAttendenceReport = json_encode(collect($attendence_bar)->values());
+
+        return view('dashboard', compact("attendences", 'attendence', 'leaves', 'clients', 'this_month_attendence', 'client_birthday_total', 'user_birthday_total', 'today_ticket', 'upcomming_ticket', 'today_income', 'u_birthday', 'c_birthday', 'today_expenditure', 'finalIncomeExpenditureReport', 'finalTaskReport', 'finalAttendenceReport'));
     }
 
 
