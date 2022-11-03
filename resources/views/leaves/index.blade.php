fd38decba3b0ede5be24a642b6585d4f5910e19c
Alija Ghimire <alijaghimire27@gmail.com>
2022-11-03 12:11:41 +0545

    delete model and icon added

--- resources/views/leaves/index.blade.php
+++ resources/views/leaves/index.blade.php
@@ -118,16 +118,28 @@
                         </td>
 
                         <td class="text-sm font-medium whitespace-nowrap">
-                            @if ($leave->status != 'approved')
-                                <form method="post" action={{ route('leaves.update', $leave->id) }}>
-                                    @csrf
-                                    @method('PUT')
-                                    <a href="{{ route('leaves.edit', $leave) }}"> <span
-                                            class="p-1 px-2 mr-2 text-white bg-blue-800 rounded"> <i
-                                                class="fa-solid fa-pen-to-square"></i> </span></a>
-                                </form>
-                            @endif
+                            <div class="flex flex-row ">
+                                @if ($leave->status != 'approved' && $leave->status != 'rejected')
+                                    <form method="post" action={{ route('leaves.update', $leave->id) }}>
+                                        @csrf
+                                        @method('PUT')
+                                        <a href="{{ route('leaves.edit', $leave) }}" title="Edit"> <span
+                                                class="p-1 px-2 mr-2 text-white bg-blue-800 rounded"> <i
+                                                    class="fa-solid fa-pen-to-square"></i> </span></a>
+                                    </form>
+                                    <a title="Delete" onclick="show({{ $leave->id }})"> <span
+                                            class="p-1 px-2 mr-2 text-white bg-red-800 rounded cursor-pointer"> <i
+                                                class="fa-solid fa-trash"></i></span></a>
+                                @endif
+                                <a title="View" href="{{ route('leaves.show', $leave) }}"> <span
+                                        class="p-1 px-2 mr-2 text-white bg-green-800 rounded"> <i
+                                            class="fa-solid fa-eye"></i>
+                                    </span></a>
+
+
+                            </div>
                         </td>
+
                     </tr>
                 @endforeach
             </tbody>
@@ -135,6 +147,26 @@
         </table>
 
 
+    </div>
+    <div id="deleteModal"
+        class="fixed top-0 bottom-0 left-0 right-0 hidden bg-black bg-opacity-25 shadow-lg backdrop-blur-lg">
+        <div class="flex items-center justify-center">
+            <div class="px-10 py-10 font-bold bg-white rounded-lg shadow-lg mt-[15%]">
+                <div>
+                    Are You sure want to Delete?
+                </div>
+                <div class="flex justify-center mt-8 space-x-5">
+                    <form action="{{ route('leaves.delete') }}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <input type="hidden" name="leave_id" id="leave_id">
+                        <button class="p-2 px-6 text-white bg-green-600 rounded-md">Yes</button>
+
+                    </form>
+                    <button onclick="hide()" class="p-2 px-6 text-white bg-red-600 rounded-md">No</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -152,5 +184,25 @@
             //     $("#btnSubmit"+id).click();
             // })
         });
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+
+        function show($id) {
+            document.getElementById('leave_id').value = $id;
+
+            $('#deleteModal').removeClass('hidden');
+        }
+
+        function hide() {
+            $('#deleteModal').addClass('hidden');
+        }
     </script>
 @endsection
