f37e9a6a45a7de5d2a5f7470f8fe5f831934f9f2
Alija Ghimire <alijaghimire27@gmail.com>
2022-11-16 08:33:50 +0545

    create error message to display

--- resources/views/tickets/edit.blade.php
+++ resources/views/tickets/edit.blade.php
@@ -9,9 +9,7 @@
             </a>
 
         </div>
-        @if ($errors->any())
-            {{ $errors }}
-        @endif
+
         <form class="w-11/12 p-6 m-6 overflow-auto bg-white rounded-md " method="POST"
             action={{ route('tickets.update', $ticket) }}>
             @csrf
@@ -25,23 +23,41 @@
                         </option>
                     @endforeach
                 </select>
+                <div class="text-red-700 ">
+                    @error('client_id')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Date</label>
                 <input type="date" class="w-full border border-gray-200 " name="date" id="date"
                     value={{ $ticket->date }}>
-
+                <div class="text-red-700 ">
+                    @error('date')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Time</label>
                 <input type="time" class="w-full border border-gray-200 " name="time" id="time"
                     value={{ $ticket->time }}>
-
+                <div class="text-red-700 ">
+                    @error('time')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Ticket No</label>
                 <input type="text" class="w-full border border-gray-200 " name="ticket_no" id="ticket_no"
                     value={{ $ticket->ticket_no }}>
+                <div class="text-red-700 ">
+                    @error('ticket_no')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
 
             <div class="mb-6">
@@ -54,6 +70,11 @@
                         </option>
                     @endforeach
                 </select>
+                <div class="text-red-700 ">
+                    @error('airline_id')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Airline Type</label>
@@ -66,29 +87,51 @@
                         International
                     </option>
                 </select>
+                <div class="text-red-700 ">
+                    @error('airline_type')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Airline Pnr</label>
                 <input type="text" class="w-full border border-gray-200 " name="airline_pnr" id="airline_pnr"
                     value={{ $ticket->airline_pnr }}>
-
+                <div class="text-red-700 ">
+                    @error('airline_pnr')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Departure</label>
                 <input type="text" class="w-full border border-gray-200 " name="departure" id="departure"
                     value={{ $ticket->departure }}>
+                <div class="text-red-700 ">
+                    @error('departure')
+                        {{ $message }}
+                    @enderror
+                </div>
 
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Destination</label>
                 <input type="text" class="w-full border border-gray-200 " name="destination" id="destination"
                     value={{ $ticket->destination }}>
-
+                <div class="text-red-700 ">
+                    @error('destination')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
             <div class="mb-6">
                 <label class="block font-bold text-gray-600 text-md">Description</label>
                 <textarea type="date" class="w-full border border-gray-200 " name="description" id="description">{{ $ticket->description }}</textarea>
-
+                <div class="text-red-700 ">
+                    @error('description')
+                        {{ $message }}
+                    @enderror
+                </div>
             </div>
 
 
@@ -96,7 +139,7 @@
 
 
             <div class="mb-6">
-                <Button class="p-2 px-4 text-white bg-indigo-600 rounded-xl">Create</Button>
+                <Button class="p-2 px-4 text-white bg-indigo-600 rounded-xl">Update</Button>
             </div>
         </form>
     </div>
